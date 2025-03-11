b89b6a01ef11fae0e6ba8e14476f793d75debf78
ducsieunhan <Chroller03@gmail.com>
2025-03-11 23:33:10 +0700

    crud

--- src/main/java/vn/ngotien/jobhunter/controller/UserController.java
+++ src/main/java/vn/ngotien/jobhunter/controller/UserController.java
@@ -1,6 +1,13 @@
 package vn.ngotien.jobhunter.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.ngotien.jobhunter.domain.User;
@@ -15,15 +22,32 @@ public class UserController {
     this.userService = userService;
   }
 
-  @GetMapping("/user/create")
-  public String createNewUser() {
-
-    User user = new User();
-    user.setEmail("chroller01@gmail.com");
-    user.setName("ducngominh");
-    user.setPassword("0209Ng");
-    this.userService.createNewUser(user);
-
-    return "create user";
+  @PostMapping("/user/create")
+  public User createNewUser(@RequestBody User postmanUser) {
+    User ducUser = this.userService.createNewUser(postmanUser);
+    return ducUser;
   }
+
+  // @DeleteMapping("/user/{id}")
+  // public void deleteUser(@PathVariable("id") long id) {
+  // this.userService.deleteUserById(id);
+  // }
+
+  @GetMapping("/user/{id}")
+  public User fetchUserById(@PathVariable("id") long id) {
+    return this.userService.getUserById(id);
+
+  }
+
+  @GetMapping("/user")
+  public List<User> fetchUserList() {
+    return this.userService.getUserList();
+  }
+
+  @PutMapping("/user")
+  public User updateUser(@RequestBody User postmanUser) {
+    User ducUser = this.userService.updateUser(postmanUser);
+    return ducUser;
+  }
+
 }
--- src/main/java/vn/ngotien/jobhunter/service/UserService.java
+++ src/main/java/vn/ngotien/jobhunter/service/UserService.java
@@ -1,5 +1,8 @@
 package vn.ngotien.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.ngotien.jobhunter.domain.User;
@@ -13,8 +16,34 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public void createNewUser(User user) {
-    this.userRepository.save(user);
+  public User createNewUser(User user) {
+    return this.userRepository.save(user);
+  }
+
+  public void deleteUserById(long id) {
+    this.userRepository.deleteById(id);
+  }
+
+  public User getUserById(long id) {
+    Optional<User> userOptional = this.userRepository.findById(id);
+    if (userOptional.isPresent()) {
+      return userOptional.get();
+    }
+    return null;
+  }
+
+  public List<User> getUserList() {
+    return this.userRepository.findAll();
+  }
+
+  public User updateUser(User user) {
+    User currentUser = this.getUserById(user.getId());
+    if (currentUser != null) {
+      currentUser.setEmail(user.getEmail());
+      currentUser.setName(user.getName());
+      currentUser.setPassword(user.getPassword());
+    }
+    return this.userRepository.save(currentUser);
   }
 
 }
