f67fe94bccba21d611211a92b2564fd157db9572
ducsieunhan <Chroller03@gmail.com>
2025-03-12 23:41:36 +0700

    053: format response before sending

--- src/main/java/vn/ngotien/jobhunter/service/error/GlobalException.java
+++ src/main/java/vn/ngotien/jobhunter/service/error/GlobalException.java
@@ -1,15 +1,45 @@
 package vn.ngotien.jobhunter.service.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import vn.ngotien.jobhunter.domain.ResResponse;
+
+/**
+ * GlobalException class is a centralized exception handler for the application.
+ * It provides a unified way to handle exceptions thrown by controllers and
+ * return appropriate HTTP responses.
+ * This class is annotated with {@link RestControllerAdvice} to indicate that it
+ * contains global exception handling methods.
+ * 
+ * @author Your Name
+ * @version 1.0
+ * @since 2025-03-13
+ */
+
 @RestControllerAdvice
 public class GlobalException {
 
+  /**
+   * Handles {@link IdInvalidException} and returns a {@link ResponseEntity} with
+   * a 400 Bad Request status.
+   * The response body contains the message from the exception.
+   * 
+   * @param idException the {@link IdInvalidException} that occurred
+   * @return a {@link ResponseEntity} with a 400 Bad Request status and the
+   *         exception message in the body
+   */
   @ExceptionHandler(value = IdInvalidException.class)
-  public ResponseEntity<String> handeIdException(IdInvalidException idException) {
-    return ResponseEntity.badRequest().body(idException.getMessage());
+  public ResponseEntity<ResResponse> handeIdException(IdInvalidException idException) {
+
+    ResResponse<Object> res = new ResResponse<>();
+    res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+    res.setError(idException.getMessage());
+    res.setMessage("IdInvalidException");
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
   }
 
 }
--- src/main/java/vn/ngotien/jobhunter/util/FormatRestResponse.java
+++ src/main/java/vn/ngotien/jobhunter/util/FormatRestResponse.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 import jakarta.servlet.http.HttpServletResponse;
+import vn.ngotien.jobhunter.domain.ResResponse;
 
 @ControllerAdvice
 public class FormatRestResponse implements ResponseBodyAdvice<Object> {
@@ -22,8 +23,21 @@ public class FormatRestResponse implements ResponseBodyAdvice<Object> {
   @Override
   public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
       Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+
     HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
-    return body;
+    int status = servletResponse.getStatus();
+
+    ResResponse<Object> res = new ResResponse<>();
+    res.setStatusCode(status);
+
+    if (status >= 400) {
+      return body;
+    } else {
+      res.setData(body);
+      res.setMessage("call api success");
+    }
+
+    return res;
   }
 
 }
